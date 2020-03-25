8740bcab1683a59ae0def873822dd6db018371f0
Xiangyu Sun <xiangyu.sun@dnata.com>
2020-03-25 17:42:24 +0800

    Initial Commit

---
+++ TianganDizhi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TianganDizhi
+//
+//  Created by 孙翔宇 on 25/03/2020.
+//  Copyright © 2020 孙翔宇. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
