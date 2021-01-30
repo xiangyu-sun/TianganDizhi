efd425bd420c30d95a354b182a1e36d4bfad099d
Xiangyu Sun <luc.alexander.sun@icloud.com>
2021-01-30 14:53:50 +0400

    add action scrtips

--- Pods/Target Support Files/Pods-ShiChenExtension/Pods-ShiChenExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ShiChenExtension : NSObject
-@end
-@implementation PodsDummy_Pods_ShiChenExtension
-@end
--- Pods/Target Support Files/Pods-TianganDizhi/Pods-TianganDizhi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TianganDizhi : NSObject
-@end
-@implementation PodsDummy_Pods_TianganDizhi
-@end
