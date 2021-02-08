a8d7411eaed70dda7e00005eee73c651c522dc9e
Xiangyu Sun <luc.alexander.sun@icloud.com>
2021-02-08 17:37:47 +0800

    add cells

---
+++ TianganDizhi/Cells.swift
@@ -0,0 +1,110 @@
+//
+//  Cells.swift
+//  TianganDizhi
+//
+//  Created by 孙翔宇 on 2021/2/8.
+//  Copyright © 2021 孙翔宇. All rights reserved.
+//
+
+import SwiftUI
+import ChineseAstrologyCalendar
+import JingluoShuxue
+import ChineseTranditionalMusicCore
+
+struct DizhiCell: View {
+    let dizhi: Dizhi
+
+    var body: some View {
+        HStack() {
+            Text(dizhi.chineseCharactor)
+            Text("(\(dizhi.chineseCharactor.transformToPinyin()))")
+        }
+        .padding()
+    }
+}
+
+struct DizhiZodiaCell: View {
+    let dizhi: Dizhi
+
+    var body: some View {
+        HStack() {
+            let zodiac = Zodiac(dizhi)
+            Text(zodiac.emoji)
+            Text(dizhi.chineseCharactor)
+            Text("\(zodiac.rawValue)")
+        }
+        .padding()
+    }
+}
+
+struct ShichenMonthCell: View {
+    let shichen: Dizhi
+    var body: some View {
+        HStack() {
+            Text(shichen.chineseCharactor)
+            Spacer()
+            Text(shichen.formattedMonth)
+        }
+        .padding()
+    }
+}
+
+
+struct ShichenHourCell: View {
+    let shichen: Dizhi
+    var body: some View {
+        HStack() {
+            Text(shichen.chineseCharactor)
+            Spacer()
+            Text(shichen.formattedHourRange ?? "")
+        }
+        .padding()
+    }
+}
+
+struct OrganShichenCell: View {
+    let shichen: Dizhi
+
+    var body: some View {
+        HStack() {
+            Text(shichen.chineseCharactor)
+                
+            Spacer()
+            Text(气血循环流注[shichen.rawValue - 1].rawValue)
+        }
+        .padding()
+    }
+}
+
+struct AliasShichenCell: View {
+    let shichen: Dizhi
+    var body: some View {
+        HStack() {
+            Text(shichen.chineseCharactor)
+            Spacer()
+            Text(shichen.aliasName)
+        }
+        .padding()
+    }
+}
+
+struct LvlvCell: View {
+    let dizhi: Dizhi
+
+    var body: some View {
+        HStack() {
+            Text(dizhi.chineseCharactor)
+                
+            Spacer()
+            
+            Text(律呂.allCases[dizhi.rawValue - 1].rawValue)
+        }
+        .padding()
+    }
+}
+
+struct Cells_Previews: PreviewProvider {
+    static var previews: some View {
+        LvlvCell(dizhi: .zi)
+    }
+}
