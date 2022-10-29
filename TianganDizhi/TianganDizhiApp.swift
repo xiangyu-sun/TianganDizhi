2dd670f61292ec880e74dd5c913355070d3fe256
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2022-10-29 22:04:15 +0200

    fixing iPhone screensize

--- TianganDizhi/TianganDizhiApp.swift
+++ TianganDizhi/TianganDizhiApp.swift
@@ -1,12 +1,18 @@
 import SwiftUI
 import ChineseAstrologyCalendar
+import Firebase
 
 @main
 struct TianganDizhiApp: App {
+    init() {
+        FirebaseApp.configure()
+        
+        UINavigationBar.appearance().largeTitleTextAttributes = [.font : UIFont(name: .weibeiBold, size: 34)!]
+        UITabBarItem.appearance().setTitleTextAttributes([.font : UIFont(name: .weibeiBold, size: 12)!], for: [])
+    }
+    
     @ObservedObject var updater = DateProvider()
-#if os(iOS)
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-#endif
+    
     var body: some Scene {
         let dizh = try? GanzhiDateConverter.shichen(Date.now)
         WindowGroup {
