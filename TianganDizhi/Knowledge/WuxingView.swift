eada7cc4e5164e91f812d4891e5e6cd372340a38
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2022-12-09 18:07:31 +0100

    WIP

--- TianganDizhi/Knowledge/WuxingView.swift
+++ TianganDizhi/Knowledge/WuxingView.swift
@@ -7,36 +7,58 @@
 //
 
 import ChineseAstrologyCalendar
+import JingluoShuxue
 import SwiftUI
 
 // MARK: - WuxingView
 
 struct WuxingView: View {
-
-  let columns = [
-    GridItem(.flexible(), spacing: 0),
-    GridItem(.flexible(), spacing: 0),
-    GridItem(.flexible(), spacing: 0),
-    GridItem(.flexible(), spacing: 0),
-    GridItem(.flexible(), spacing: 0),
-  ]
-
-  var body: some View {
-    ScrollView {
-      LazyVGrid(columns: columns, alignment: .center, spacing: 0) {
-        ForEach(Wuxing.allCases, id: \.self) { item in
-          Text("\(item.chineseCharacter)")
+    
+    let columns = [
+        GridItem(.flexible(), spacing: 0)
+    ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, alignment: .center, spacing: 20) {
+                ForEach(Wuxing.allCases, id: \.self) { item in
+                    HStack() {
+                        Text("\(item.chineseCharacter)")
+                            .font(.title2)
+                        
+                        HStack(){
+                            Text("\(item.tiangan.0.chineseCharactor)")
+                            Text("\(item.tiangan.1.chineseCharactor)")
+                        }
+                        
+                        HStack(){
+                            ForEach(item.dizhi) {
+                                Text("\($0.chineseCharactor)")
+                            }
+                        }
+                        
+                        HStack() {
+                            let wuzang = 五臟.allCases.first{ $0.wuxing == item } ?? .心
+                            Text("\(wuzang.rawValue)")
+                            Text("\(wuzang.情緒)")
+                        }
+                    }
+                }
+                
+                ForEach(五臟.allCases, id: \.self) { item in
+                  
+                }
+      
+            }
+            .padding()
         }
-      }
-      .padding()
     }
-  }
 }
 
 // MARK: - WuxingView_Previews
 
 struct WuxingView_Previews: PreviewProvider {
-  static var previews: some View {
-    WuxingView()
-  }
+    static var previews: some View {
+        WuxingView()
+    }
 }
