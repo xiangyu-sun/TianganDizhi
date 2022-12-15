6c1dd08c742ff7120de88d89e537051d1c884437
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2022-12-15 10:55:36 +0100

    lint code

--- TianganDizhi/Knowledge/WuxingView.swift
+++ TianganDizhi/Knowledge/WuxingView.swift
@@ -13,71 +13,69 @@ import SwiftUI
 // MARK: - WuxingView
 
 struct WuxingView: View {
-    @Environment(\.bodyFont) var bodyFont
-    @Environment(\.title2Font) var title2Font
-    @Environment(\.footnote) var footnote
-    
-    let columns = [
-        GridItem(.flexible(), spacing: 0)
-    ]
-    
-    var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, alignment: .leading, spacing: 8) {
-                ForEach(Wuxing.allCases, id: \.self) { item in
-                    HStack() {
-                        VStack(alignment: .leading) {
-                            Text("\(item.chineseCharacter)")
-                                .font(title2Font)
-                            HStack() {
-                                
-                                VStack() {
-                                    Text("天干")
-                                        .font(footnote)
-                                    HStack(){
-                                        Text("\(item.tiangan.0.chineseCharactor)")
-                                        Text("\(item.tiangan.1.chineseCharactor)")
-                                    }
-                                }
-                                Divider()
-                                
-                                VStack() {
-                                    Text("地支")
-                                        .font(footnote)
-                                    HStack(){
-                                        ForEach(item.dizhi) {
-                                            Text("\($0.chineseCharactor)")
-                                        }
-                                    }
-                                }
-                                
-                                Divider()
-                                
-                                VStack() {
-                                    Text("臟腑和情緒")
-                                        .font(footnote)
-                                    HStack() {
-                                        let wuzang = 五臟.allCases.first{ $0.wuxing == item } ?? .心
-                                        Text("\(wuzang.rawValue)")
-                                        Text("\(wuzang.情緒)")
-                                    }
-                                }
-                                
-                            }
-                            .font(bodyFont)
-                        }
-                    }
+  @Environment(\.bodyFont) var bodyFont
+  @Environment(\.title2Font) var title2Font
+  @Environment(\.footnote) var footnote
+
+  let columns = [
+    GridItem(.flexible(), spacing: 0),
+  ]
+
+  var body: some View {
+    ScrollView {
+      LazyVGrid(columns: columns, alignment: .leading, spacing: 8) {
+        ForEach(Wuxing.allCases, id: \.self) { item in
+          HStack {
+            VStack(alignment: .leading) {
+              Text("\(item.chineseCharacter)")
+                .font(title2Font)
+              HStack {
+                VStack {
+                  Text("天干")
+                    .font(footnote)
+                  HStack {
+                    Text("\(item.tiangan.0.chineseCharactor)")
+                    Text("\(item.tiangan.1.chineseCharactor)")
+                  }
                 }
+                Divider()
+
+                VStack {
+                  Text("地支")
+                    .font(footnote)
+                  HStack {
+                    ForEach(item.dizhi) {
+                      Text("\($0.chineseCharactor)")
+                    }
+                  }
+                }
+
+                Divider()
+
+                VStack {
+                  Text("臟腑和情緒")
+                    .font(footnote)
+                  HStack {
+                    let wuzang = 五臟.allCases.first { $0.wuxing == item } ?? .心
+                    Text("\(wuzang.rawValue)")
+                    Text("\(wuzang.情緒)")
+                  }
+                }
+              }
+              .font(bodyFont)
             }
-            .padding()
+          }
         }
+      }
+      .padding()
     }
+  }
 }
 
 // MARK: - WuxingView_Previews
 
 struct WuxingView_Previews: PreviewProvider {
-    static var previews: some View {
-        WuxingView()
-    }
+  static var previews: some View {
+    WuxingView()
+  }
 }
