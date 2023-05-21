78b07f7931451207f67c7949a2bb8e24ce8faaea
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2023-05-21 20:23:23 +0200

    add jieqi view

--- TianganDizhi/ContentView.swift
+++ TianganDizhi/ContentView.swift
@@ -30,11 +30,11 @@ struct ContentView: View {
           Text("天干地支")
         }
 
-      //            JieqiContainerView(padding: 0)
-      //            .tabItem {
-      //                Image(systemName: "sun.max.fill")
-      //                Text("二十四節氣")
-      //            }
+      JieqiContainerView()
+        .tabItem {
+          Image(systemName: "sun.max.fill")
+          Text("二十四節氣")
+        }
 
       ChartListView()
         .tabItem {
--- TianganDizhi/JieqiContainerView.swift
+++ TianganDizhi/JieqiContainerView.swift
@@ -7,27 +7,74 @@
 //
 
 import SwiftUI
-
-// MARK: - JieqiContainerView
+import ChineseAstrologyCalendar
 
 struct JieqiContainerView: View {
-
-  let padding: CGFloat
-
+  @State private var rotation: Double = 0.0
+  @GestureState private var dragOffset: CGSize = .zero
+  
+  @Environment(\.bodyFont) var bodyFont
+  @Environment(\.footnote) var footnote
+  
+  let numberOfItems = 24
+  let rotationSensitivity: Double = 2.0 // Adjust the rotation sensitivity here
+  
   var body: some View {
-    ZStack {
-      JieqiCircularView(currentJieqi: nil)
-        .scaledToFill()
+    NavigationView() {
+      GeometryReader { geometry in
+        ZStack {
+          ForEach(Jieqi.allCases, id: \.self) { jieqi in
+            Text(jieqi.chineseName)
+              .font(bodyFont)
+              .cornerRadius(4)
+              .offset(anglePosition(for: jieqi.rawValue - 1, in: geometry.size))
+          }
+        }
+        .frame(width: geometry.size.width, height: geometry.size.height)
+        .overlay(
+          Color.clear
+            .contentShape(Rectangle())
+            .gesture(
+              DragGesture()
+                .updating($dragOffset) { value, state, _ in
+                  state = value.translation
+                }
+                .onChanged { value in
+                  let dragDistance = sqrt(pow(value.translation.width, 2) + pow(value.translation.height, 2))
+                  let diagonalLength = sqrt(pow(geometry.size.width, 2) + pow(geometry.size.height, 2))
+                  
+                  
+                  let uppertHalf = value.startLocation.y <= (geometry.size.height * 0.5)
+                  
+                  
+                  let touchOffsetX = Double((uppertHalf ? -value.translation.width : value.translation.width) / diagonalLength)
+                  
+                  let rightHalf = value.startLocation.x >= (geometry.size.width * 0.5)
+                  
+                  let touchOffsetY = Double((rightHalf ? -value.translation.height : value.translation.height) / diagonalLength)
+                  
+                  rotation -= (touchOffsetX + touchOffsetY) * 2 * .pi * rotationSensitivity / Double(dragDistance)
+                }
+            )
+        )
+      }
+      .navigationTitle("二十四節氣")
     }
-    .padding(padding)
-    .scaledToFit()
+  }
+  
+  private func anglePosition(for index: Int, in size: CGSize) -> CGSize {
+    let segmentAngle = 2 * .pi / Double(numberOfItems)
+    let rotationOffset = rotation.truncatingRemainder(dividingBy: 2 * .pi)
+    let startAngle = -segmentAngle * Double(index) + rotationOffset
+    let radius = min(size.width, size.height) / 2 - 30 // Adjust the spacing between the circle and the Text elements here
+    let x = cos(startAngle) * radius
+    let y = sin(startAngle) * radius
+    return CGSize(width: x, height: y)
   }
 }
 
-// MARK: - JieqiContainerView_Previews
-
 struct JieqiContainerView_Previews: PreviewProvider {
   static var previews: some View {
-    JieqiContainerView(padding: 0)
+    JieqiContainerView()
   }
 }
