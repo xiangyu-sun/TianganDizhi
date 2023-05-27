8fdb32a88101e84ada302b25797e2b49f58b4e04
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2023-05-27 15:02:15 +0200

    fixing date color update

--- TianganDizhi/Utilities/DateColorModifier.swift
+++ TianganDizhi/Utilities/DateColorModifier.swift
@@ -4,18 +4,19 @@ import SwiftUI
 
 struct AutoColorPastDate: ViewModifier {
   let date: Date?
+  let now: Date
 
   func body(content: Content) -> some View {
     guard let date else {
       return content.foregroundColor(.primary)
     }
     return content
-      .foregroundColor(date <= Date() ? .secondary : .primary)
+      .foregroundColor(date <= now ? .secondary : .primary)
   }
 }
 
 extension View {
-  func autoColorPastDate(_ date: Date?) -> some View {
-    modifier(AutoColorPastDate(date: date))
+  func autoColorPastDate(_ date: Date?, now: Date = Date()) -> some View {
+    modifier(AutoColorPastDate(date: date, now: now))
   }
 }
