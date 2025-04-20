39961f1842826a586871ac64b33d52b1b1d09fc3
Xiangyu  Sun <Xiangyu.sun@glovoapp.com>
2025-04-20 18:21:05 +0200

    fixing mac build

--- ShiChen/Shared/IntentTimelineProvider/MinuteTimelineProvider.swift
+++ ShiChen/Shared/IntentTimelineProvider/MinuteTimelineProvider.swift
@@ -34,6 +34,7 @@ struct MinuteTimelineProvider: IntentTimelineProvider {
     completion(timeline)
   }
   
+  @available(macOSApplicationExtension 15.0, *)
   @available(watchOSApplicationExtension 11.0, *)
   @available(iOSApplicationExtension 18.0, *)
   func relevance() async -> WidgetRelevance<ConfigurationIntent> {
