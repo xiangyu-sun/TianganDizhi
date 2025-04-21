554cbb9dc3d0036e48750ef45a54da8e4e85cbb5
Xiangyu  Sun <Xiangyu.sun@glovoapp.com>
2025-04-21 13:51:22 +0200

    adding next Jieqi for widget

--- ShiChen/JieqiWidget.swift
+++ ShiChen/JieqiWidget.swift
@@ -10,8 +10,10 @@ import WidgetKit
 import ChineseAstrologyCalendar
 import Intents
 import SwiftUI
+import Astral
 
  // MARK: - Jieqi
+
 
 struct JieqiWidget: Widget {
   let kind = "Jieqi"
@@ -20,17 +22,48 @@ struct JieqiWidget: Widget {
     return [.systemSmall]
   }
 
+  func getDate(date: Date) -> Date {
+
+    let nextDate = preciseNextSolarTermDate(from: date)
+    
+    let interval = nextDate.timeIntervalSince(date)
+    let days = Int(ceil(interval / 86_400))  // floor of full days
+    
+    if days <= 14 {
+      return nextDate
+    } else {
+      return date
+    }
+    
+  }
 
   var body: some WidgetConfiguration {
     IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: JieqiTimelineProvider()) { entry in
-      if let jieqi = entry.date.jieqi {
+      let date = getDate(date: entry.date)
+      let sameDate = date == entry.date
+      
+      if let jieqi = date.jieqi {
         VStack(alignment: .center) {
-          Text(entry.date, style: .date)
-            .font(.callout)
-            .environment(\.locale, .init(identifier: "zh-Hant"))
-   
-          Text(jieqi.chineseName)
-            .font(largeTitleFont)
+          if sameDate {
+            Text(date, style: .date)
+              .font(.callout)
+              .environment(\.locale, .init(identifier: "zh-Hant"))
+            
+            Text(jieqi.chineseName)
+              .font(largeTitleFont)
+          } else {
+            Text(entry.date, style: .date)
+              .font(.callout)
+              .environment(\.locale, .init(identifier: "zh-Hant"))
+            Text(date, style: .date)
+              .font(.callout)
+              .foregroundColor(.secondary)
+              .environment(\.locale, .init(identifier: "zh-Hant"))
+            
+            Text(jieqi.chineseName)
+              .foregroundColor(.secondary)
+              .font(largeTitleFont)
+          }
         }
         .frame(maxWidth: .infinity)
         .materialBackgroundWidget(with: Image(uiImage: jieqi.image))
